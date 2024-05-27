e57464c23ebb7bc9f4f8e0781171dc56c2bb9c69
Ansh Mehta <anshkapilmehta@gmail.com>
2024-05-27 14:25:29 +0530

    aligment of canvas and heading

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import AnimatedHeading from "@/components/AnimatedHeading";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col bg-lime-400">
+    <main className="w-screen h-screen flex flex-col bg-cyan-400">
       <AnimatedHeading text={"Hi I'm Ansh, and this is my portfolio"} />
       <GameCanvas />
     </main>
